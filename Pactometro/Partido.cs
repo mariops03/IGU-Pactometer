ebcbe620b1dc41c863235c8cd3326875d022f5d7
mariops <marioprieta@gmail.com>
2023-11-07 20:48:07 +0100

    Clases hechas

---
+++ Pactometro/Partido.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pactometro
+{
+    internal class Partido
+    {
+        string nombre { get; set; }
+        int escaños { get; set; }
+        String color { get; set; }
+        public Partido()
+        {
+
+        }
+
+        public Partido(string nombre, int escaños, String color)
+            {
+                this.nombre = nombre;
+                this.escaños = escaños;
+                this.color = color;
+            }
+
+        public override string ToString()
+        {
+            return nombre + " " + escaños;
+        }
+    }
+
+    
+}
---
+++ Pactometro/ProcesoElectoral.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pactometro
+{
+    internal class ProcesoElectoral
+    {
+        public string nombre { get; set; }
+        public DateTime fecha { get; set; }
+        public int numEscaños { get; set; }
+        public Collection<Partido> Partidos = new Collection<Partido>
+        public int mayoriaAbsoluta { get; set; }
+
+        public ProcesoElectoral()
+        {
+
+        }
+        public ProcesoElectoral(string nombre, DateTime fecha, int numEscaños, int mayoriaAbsoluta)
+        {
+            this.nombre = nombre;
+            this.fecha = fecha;
+            this.numEscaños = numEscaños;
+            this.mayoriaAbsoluta = mayoriaAbsoluta;
+        }
+
+        public override string ToString()
+        {
+            return nombre + " " + fecha + "" + numEscaños + "" + mayoriaAbsoluta;
+        }
+    }
+}
